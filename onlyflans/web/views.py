59f8b358ef6eec4123d79a601f034412e0f10b6d
Néstor Patricio Rojas Ríos <np.rojas@gmail.com>
2022-03-22 16:03:04 -0300

    A form to contact with clients was created.

--- onlyflans/web/views.py
+++ onlyflans/web/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
-from .models import Flan
+from django.http import HttpResponseRedirect
+
+from .models import ContactForm, Flan
+from .forms import ContactFormForm
 
 
 def index(request):
     public_flans = Flan.objects.filter(is_private = False)
-    return render(request, './web/index.html', {'flans': public_flans})
+    return render(request, './web/index.html', {'public_flans': public_flans})
 
 def acerca(request):
     return render(request, './web/about.html', {})
 
 def bienvenido(request):
     private_flans = Flan.objects.filter(is_private = True)
-    return render(request, './web/welcome.html', {'flans': private_flans})
+    return render(request, './web/welcome.html', {'private_flans': private_flans})
 
 def contacto(request):
-    return render(request, './web/contact.html', {})
+    if request.method == 'POST':
+        form = ContactFormForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/web/')
+    else:
+        form = ContactFormForm()
+    return render(request, './web/contact.html', {'form': form})
