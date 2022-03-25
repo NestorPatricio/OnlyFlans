415c499e444394b1168a38be9ba24c9b580350b9
Néstor Patricio Rojas Ríos <np.rojas@gmail.com>
2022-03-25 14:34:44 -0300

    Creating a register page and a searcher.

--- onlyflans/web/urls.py
+++ onlyflans/web/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
-from .views import index, acerca, bienvenido, contacto
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from .views import index, acerca, bienvenido, contacto, exito, registro, buscaflanes
 
 app_name = 'web'
 
@@ -11,5 +13,15 @@ urlpatterns = [
     # example: /web/bienvenido/
     path('bienvenido', bienvenido, name = 'bienvenida'),
     # example: /web/contacto/
-    path('contacto', contacto, name = 'contáctanos')
+    path('contacto', contacto, name = 'contáctanos'),
+    # example: /web/exito/
+    path('exito', exito, name = 'éxito'),
+    # example: /web/registro/
+    path('registrar', registro, name = 'regístrate'),
+    # example: /web/accounts/login/
+    path('accounts/login', auth_views.LoginView.as_view(template_name = 'web/registration/login.html'), name = 'login'),
+    # example: /web/accounts/logout/
+    path('accounts/logout', auth_views.LogoutView.as_view(template_name = 'web/registration/logged_out.html'), name = 'logout'),
+    # example: /web/buscaflan/
+    path('buscaflan', buscaflanes, name = 'búscalo')
 ]
--- onlyflans/web/views.py
+++ onlyflans/web/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 from .models import ContactForm, Flan
-from .forms import ContactFormForm
+from .forms import ContactFormForm, ContactFormModelForm, UserRegisterForm
 
 
 def index(request):
@@ -12,15 +16,52 @@ def index(request):
 def acerca(request):
     return render(request, './web/about.html', {})
 
+@login_required
 def bienvenido(request):
     private_flans = Flan.objects.filter(is_private = True)
     return render(request, './web/welcome.html', {'private_flans': private_flans})
 
 def contacto(request):
     if request.method == 'POST':
-        form = ContactFormForm(request.POST)
+        form = ContactFormModelForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/web/')
+            contact = ContactForm.objects.create(**form.cleaned_data)
+            return HttpResponseRedirect('/web/exito')
     else:
-        form = ContactFormForm()
+        form = ContactFormModelForm()
     return render(request, './web/contact.html', {'form': form})
+
+def exito(request):
+    return render(request, './web/success.html', {})
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            usuario = form.cleaned_data['username']
+            contrasena = form.cleaned_data['password1']
+            new_user = authenticate(username = usuario, password = contrasena)
+            login(request, user = new_user)
+            messages.success(request, f'Usuario {usuario} registrado exitosamente.')
+            return redirect('/web/')
+    else:
+        form = UserRegisterForm()
+    return render(request, './web/register.html', {'form': form})
+
+def buscaflanes(request):
+    if request.method == 'POST':
+        # 'buscador' guarda el valor ingresado en el formulario de búsqueda.
+        buscador = request.POST['buscador']
+        # Filtrar los distintos objetos flanes para que coincidan con nuestro
+        # criterio de búsqueda. En este caso, con el nombre de cada flan.
+        flanes = Flan.objects.filter(name__contains = buscador)
+        return render(request, './web/results_flans.html', {
+            'buscador': buscador,
+            'flanes': flanes,
+            })
+        
+    else:
+        return render(request, './web/results_flans.html', {})
+    
+    
